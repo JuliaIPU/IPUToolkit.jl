14e17b10ddda665a363c66a4f8681b3f747972de
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-02-01 00:25:31 +0100

    Add more extremely basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,17 @@ using Poplar
 
 @testset "Poplar.jl" begin
     @test Poplar.Tensor().cpp_object != C_NULL
+    dm = Poplar.DeviceManager()
+    @test Poplar.DeviceManagerGetNumDevices(dm) > 0
+
+    # Some simple code (NOTE: very incomplete at the moment)
+    model = Poplar.IPUModel()
+    @test model.cpp_object != C_NULL
+    device = Poplar.IPUModelCreateDevice(model)
+    @test device.cpp_object != C_NULL
+    target = Poplar.DeviceGetTarget(device)
+    @test target.cpp_object != C_NULL
+    graph = Poplar.Graph(target)
+    @test graph.cpp_object != C_NULL
+    Poplar.PopopsAddCodelets(graph)
 end
