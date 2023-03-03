3e9289f4eee909f4123e47821efa56a10b0b34e6
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-03-03 01:11:48 +0100

    Update LLVM builder code

--- examples/llvm_builder.jl
+++ examples/llvm_builder.jl
@@ -1,27 +1,27 @@
 # based on GPUCompiler example https://github.com/JuliaGPU/GPUCompiler.jl/blob/master/examples/kernel.jl
 module llvmBuilder
+
 using GPUCompiler
 using StaticArrays
 using Match
 
 module IpuRuntime
 
+# dummy methods
+signal_exception() = return
 
-    # dummy methods
-    signal_exception() = return
+# Todo: box/unbox for allowing proper type conversion
+# https://github.com/JuliaGPU/CUDAnative.jl/blob/a15d2db96274948d8090457a001e62e14be0d883/src/device/runtime.jl
+#function malloc(sz::Csize_t)
+#    return ccall("extern fake_malloc", llvmcall, Int64, (Csize_t,), sz)
+#end
 
-    # Todo: box/unbox for allowing proper type conversion
-    # https://github.com/JuliaGPU/CUDAnative.jl/blob/a15d2db96274948d8090457a001e62e14be0d883/src/device/runtime.jl
-    #function malloc(sz::Csize_t) 
-    #    return ccall("extern fake_malloc", llvmcall, Int64, (Csize_t,), sz)
-    #end
+report_oom(sz) = return
+report_exception(ex) = return
+report_exception_name(ex) = return
+report_exception_frame(idx, func, file, line) = return
 
-
-    report_oom(sz) = return
-    report_exception(ex) = return
-    report_exception_name(ex) = return
-    report_exception_frame(idx, func, file, line) = return
-end
+end # module IpuRuntime
 
 struct TestCompilerParams <: AbstractCompilerParams end
 GPUCompiler.runtime_module(::CompilerJob{<:Any,TestCompilerParams}) = IpuRuntime
@@ -64,11 +64,10 @@ end
     return PoplarVec{T, T2}(ccall(arg, llvmcall, Ptr{T}, (Int32, Int32), i, 0), ccall(arg, llvmcall, UInt32, (Int32, Int32), i, 1))
 end
 
-
 macro Codelet(name, usr_kern)
     func = eval(usr_kern)
     args = methods(func).ms[end].sig.parameters[2:end]
-    
+
     kernargs = []
     i = Int32(0)
 
@@ -81,7 +80,7 @@ macro Codelet(name, usr_kern)
     kern_call = Expr(:call, :($func), kernargs...)
 
     return quote
-        
+
         function $(esc(name))()
             $kern_call
             return nothing
@@ -89,7 +88,6 @@ macro Codelet(name, usr_kern)
         $buildCodelet($(esc(name)), String(nameof($(esc(name)))), $usr_kern)
     end
 end
-
 
 function buildCodelet(kernel, name, origKernel=function ()end)
     args = methods(origKernel).ms[end].sig.parameters[2:end]
@@ -100,10 +98,10 @@ function buildCodelet(kernel, name, origKernel=function ()end)
     open("gen_codelet.txt", "w") do io
         for i in 1:length(args)
             @match args[i] begin
-            PoplarVec{Int32, In} => write(io, "poplar::Input<poplar::Vector<int>> $(argnames[i]);\n")
-            PoplarVec{Float32, In} => write(io, "poplar::Input<poplar::Vector<float>> $(argnames[i]);\n")
-            PoplarVec{Int32, Out} => write(io, "poplar::Output<poplar::Vector<int>> $(argnames[i]);\n")
-            PoplarVec{Float32, Out} => write(io, "poplar::Output<poplar::Vector<float>> $(argnames[i]);\n")
+                PoplarVec{Int32, In} => write(io, "poplar::Input<poplar::Vector<int>> $(argnames[i]);\n")
+                PoplarVec{Float32, In} => write(io, "poplar::Input<poplar::Vector<float>> $(argnames[i]);\n")
+                PoplarVec{Int32, Out} => write(io, "poplar::Output<poplar::Vector<int>> $(argnames[i]);\n")
+                PoplarVec{Float32, Out} => write(io, "poplar::Output<poplar::Vector<float>> $(argnames[i]);\n")
 
                 PoplarVec{Int32, InOut} => write(io, "poplar::InOut<poplar::Vector<int>> $(argnames[i]);\n")
                 PoplarVec{Float32, InOut} => write(io, "poplar::InOut<poplar::Vector<float>> $(argnames[i]);\n")
@@ -111,24 +109,25 @@ function buildCodelet(kernel, name, origKernel=function ()end)
         end
     end
 
-
     outputPath = name * ".gp"
     source = FunctionSpec(kernel)
 
     target = NativeCompilerTarget()
     params = TestCompilerParams()
-    job = CompilerJob(target, source, params)
+    job = CompilerJob(target, source, params, :func)
 
-    
-    output = ""
-    try
-        output = string(GPUCompiler.compile(:llvm, job)[1])
+    output = try
+        JuliaContext() do ctx
+            string(GPUCompiler.compile(:llvm, job; ctx)[1])
+        end
     catch
-        GPUCompiler.@device_code_warntype GPUCompiler.compile(:llvm, job)
+        JuliaContext() do ctx
+            GPUCompiler.@device_code_warntype GPUCompiler.compile(:llvm, job; ctx)
+        end
+        ""
     end
 
-    kernel_name = match(Regex("(_Z[0-9]+julia_$(Symbol(kernel))_[0-9]+)"), output)[1]
-
+    kernel_name = match(Regex("(julia_$(Symbol(kernel))_[0-9]+)"), output)[1]
 
     open("julia-code.ll", "w") do io
         write(io, output)
@@ -137,4 +136,4 @@ function buildCodelet(kernel, name, origKernel=function ()end)
     run(`popc -g -O0 -X -Wno-override-module -X -Qunused-arguments -DGET_VEC_NAME=getVec$name -DCLASS_NAME=$name -DFIRST_NAME=$(argnames[1]) -DKERNEL_NAME=$kernel_name julia-code.ll codelet_gen.cpp -o $outputPath`)
 end
 
-end
+end # module llvmBuilder
--- examples/main.jl
+++ examples/main.jl
@@ -1,21 +1,23 @@
 include(joinpath(@__DIR__, "llvm_builder.jl"))
-using Poplar
+using IPUToolkit.Poplar
 using GPUCompiler
 
 ##
 
-@llvmBuilder.Codelet TimesTwo function (inconst::llvmBuilder.PoplarVec{Float32, llvmBuilder.In}, outvec::llvmBuilder.PoplarVec{Float32, llvmBuilder.Out})
+llvmBuilder.@Codelet TimesTwo function (inconst::llvmBuilder.PoplarVec{Float32, llvmBuilder.In}, outvec::llvmBuilder.PoplarVec{Float32, llvmBuilder.Out})
     (outvec .= inconst .* 2)
 end
 
-@llvmBuilder.Codelet Sort function (invec::llvmBuilder.PoplarVec{Float32, llvmBuilder.In}, outvec::llvmBuilder.PoplarVec{Float32, llvmBuilder.Out})
+llvmBuilder.@Codelet Sort function (invec::llvmBuilder.PoplarVec{Float32, llvmBuilder.In}, outvec::llvmBuilder.PoplarVec{Float32, llvmBuilder.Out})
     outvec .= invec
     sort!(outvec)
 end
 
-model = Poplar.IPUModel()
-device = Poplar.IPUModelCreateDevice(model)
-# device = Poplar.getIPU()
+device = if Poplar.SDK_VERSION < v"2.0"
+    Poplar.IPUModelCreateDevice(Poplar.IPUModel())
+else
+    Poplar.get_ipu_device()
+end
 
 target = Poplar.DeviceGetTarget(device)
 graph = Poplar.Graph(target)
@@ -23,9 +25,9 @@ graph = Poplar.Graph(target)
 Poplar.GraphAddCodelets(graph, "TimesTwo.gp")
 Poplar.GraphAddCodelets(graph, "Sort.gp")
 
-inconst = Poplar.GraphAddConstant(graph, Poplar.FLOAT, UInt64[10], Float32[5, 2, 10, 102, -10, 2, 256, 15, 32, 100])
-outvec1 = Poplar.GraphAddVariable(graph, Poplar.FLOAT, UInt64[10], "outvec1");
-outvec2 = Poplar.GraphAddVariable(graph, Poplar.FLOAT, UInt64[10], "outvec2");
+inconst = Poplar.GraphAddConstant(graph, Poplar.FLOAT(), UInt64[10], Float32[5, 2, 10, 102, -10, 2, 256, 15, 32, 100])
+outvec1 = Poplar.GraphAddVariable(graph, Poplar.FLOAT(), UInt64[10], "outvec1");
+outvec2 = Poplar.GraphAddVariable(graph, Poplar.FLOAT(), UInt64[10], "outvec2");
 
 Poplar.GraphSetTileMapping(graph, inconst, 0)
 Poplar.GraphSetTileMapping(graph, outvec1, 0)
@@ -38,15 +40,22 @@ TimesTwoVtx = Poplar.GraphAddVertex(graph, computeSetMul, "TimesTwo")
 Poplar.GraphConnect(graph, TimesTwoVtx["inconst"], inconst)
 Poplar.GraphConnect(graph, TimesTwoVtx["outvec"], outvec1)
 Poplar.GraphSetTileMapping(graph, TimesTwoVtx, 0)
-Poplar.GraphSetCycleEstimate(graph, TimesTwoVtx, 1)
+if Poplar.SDK_VERSION < v"2.0"
+    Poplar.GraphSetCycleEstimate(graph, TimesTwoVtx, 1)
+else
+    # Poplar.GraphSetPerfEstimate(graph, TimesTwoVtx, 1)
+end
 
 computeSetSort = Poplar.GraphAddComputeSet(graph, "computeSetSort")
 SortVtx = Poplar.GraphAddVertex(graph, computeSetSort, "Sort")
 Poplar.GraphConnect(graph, SortVtx["invec"], outvec1)
 Poplar.GraphConnect(graph, SortVtx["outvec"], outvec2)
 Poplar.GraphSetTileMapping(graph, SortVtx, 0)
-Poplar.GraphSetCycleEstimate(graph, SortVtx, 1)
-
+if Poplar.SDK_VERSION < v"2.0"
+    Poplar.GraphSetCycleEstimate(graph, SortVtx, 1)
+else
+    # Poplar.GraphSetPerfEstimate(graph, SortVtx, 1)
+end
 
 Poplar.ProgramSequenceAdd(prog, Poplar.ProgramExecute(computeSetMul))
 Poplar.ProgramSequenceAdd(prog, Poplar.ProgramExecute(computeSetSort))
@@ -59,3 +68,4 @@ Poplar.OptionFlagsSet(flags, "debug.instrument", "true")
 
 engine = Poplar.Engine(graph, prog, flags)
 Poplar.EngineLoadAndRun(engine, device)
+Poplar.DeviceDetach(device)
