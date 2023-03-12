d97106c5e414d94fe06de17bed605b3e11a50218
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-03-12 13:22:11 +0100

    Make code generation work correctly in Julia v1.9
    The main problem was that the `ccall` to the extern `getVec` function was trying
    to return either an `i64` or `i32`, depending on the value of one of the
    arguments (`i64` for the pointer of the array, `i32` for its size).  The
    solution was to split this function into two, one returning `uintptr_t`/`i64`,
    and the other returning `uint32_t`/`i32`, so that the `ccall` can have a more
    straightforward signature.
    
    Another problem that arise in Julia v1.9 was that the new default algorithm of
    `sort!` requires a scratch array, which we can't allocate on the IPU, so we need
    to use the simpler `QuickSort` algorithm.  Also, to use this function we had to
    increase the stack size of the kernel in the codelet.
    
    Fix #8.

--- src/codelet_gen.cpp
+++ src/codelet_gen.cpp
@@ -1,18 +1,21 @@
 extern "C" {
 #include <stdint.h>
-extern void KERNEL_NAME();
-struct PoplarVector {
-  uint32_t beginPtr;
-  uint32_t size_;
-};
-static PoplarVector* vertclass;
+    extern void KERNEL_NAME();
+    struct PoplarVector {
+        uintptr_t beginPtr;
+        uint32_t  size_;
+    };
+    static PoplarVector* vertclass;
 
-uint32_t GET_VEC_NAME(uint32_t i, uint32_t type) {
-  if(type == 0)
-    return vertclass[i].beginPtr;
-  return vertclass[i].size_;
-}
+    // Function to get the pointer to the i-th vector.
+    uintptr_t GET_VEC_PTR_NAME(int32_t i) {
+        return vertclass[i].beginPtr;
+    }
 
+    // Function to get the size of the i-th vector.
+    uint32_t GET_VEC_SIZE_NAME(int32_t i) {
+        return vertclass[i].size_;
+    }
 }
 
 #include <poplar/Vertex.hpp>
@@ -22,8 +25,9 @@ uint32_t GET_VEC_NAME(uint32_t i, uint32_t type) {
 #ifdef DEF_STACK_USAGE
 static_assert(sizeof(void*) == 4);
 
-DEF_STACK_USAGE(0x1000, KERNEL_NAME);
-DEF_STACK_USAGE(0x1000, GET_VEC_NAME);
+DEF_STACK_USAGE(0x2000, KERNEL_NAME);
+DEF_STACK_USAGE(0x1000, GET_VEC_PTR_NAME);
+DEF_STACK_USAGE(0x1000, GET_VEC_SIZE_NAME);
 #endif
 
 class CLASS_NAME : public poplar::Vertex {
@@ -39,13 +43,13 @@ struct VectorBase {
 
   // Fields
   //poplar::InOut<poplar::Vector<int>> out;
-  #include "gen_codelet.txt"
+#include "gen_codelet.cpp"
 
   // Compute function
-  bool compute() {
-    vertclass = (PoplarVector*) &FIRST_NAME;
+    bool compute() {
+        vertclass = (PoplarVector*) &FIRST_NAME;
 
-    KERNEL_NAME();
-    return true;
-  }
+        KERNEL_NAME();
+        return true;
+    }
 };
