ed1d73cde150f5a4fc7d9733a64e959a9f695972
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-05-19 18:01:09 +0200

    Generalise some code to multi-dimensional tensors
    Note that in practice we are still limited to 1D tensors (vectors).

--- examples/dsl.jl
+++ examples/dsl.jl
@@ -1,12 +1,12 @@
 using IPUToolkit.IPUCompiler, IPUToolkit.Poplar
 
 # Define the arrays that will be used during the program.  `input` is a host array that will
-# be automatically copied to an IPU array, the other `PoplarVec`s are placeholders for IPU
-# arrays that will be populated during the execution of the program.
+# be automatically copied to an IPU array, the other `PoplarVector`s are placeholders for
+# IPU arrays that will be populated during the execution of the program.
 input = Float32[5, 2, 10, 102, -10, 2, 256, 15, 32, 100]
-outvec1 = PoplarVec{Float32, Out}(undef, 10)
-outvec2 = PoplarVec{Float32, Out}(undef, 10)
-outvec3 = PoplarVec{Float32, Out}(undef, 10)
+outvec1 = PoplarVector{Float32, Out}(undef, 10)
+outvec2 = PoplarVector{Float32, Out}(undef, 10)
+outvec3 = PoplarVector{Float32, Out}(undef, 10)
 
 # Get the device.
 device = Poplar.get_ipu_device()
@@ -18,22 +18,22 @@ device = Poplar.get_ipu_device()
 # * print tensors on the IPU with the "special" function `print_tensor`
 # * copy IPU tensors to the host
 @ipuprogram device begin
-    # Define the functions/codelets.  All arguments must be `PoplarVec`s.
-    function TimesTwo(inconst::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+    # Define the functions/codelets.  All arguments must be `PoplarVector`s.
+    function TimesTwo(inconst::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
         outvec .= 2 .* inconst
     end
-    function Sort(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+    function Sort(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
         outvec .= invec
         sort!(outvec)
     end
-    function Sin(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+    function Sin(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
         for idx in eachindex(outvec)
             @inbounds outvec[idx] = sin(invec[idx])
         end
     end
 
     # Run the functions.  Arguments must be the arrays defined above, either host arrays
-    # (which will be copied to the IPU automatically) or `PoplarVec`s.
+    # (which will be copied to the IPU automatically) or `PoplarVector`s.
     TimesTwo(input, outvec1)
     Sort(outvec1, outvec2)
     Sin(outvec2, outvec3)
--- test/compiler.jl
+++ test/compiler.jl
@@ -13,22 +13,22 @@ function test_compiler_program(device)
 
     # Define a local function to make sure macro hygiene is right
     double(x) = x * 2
-    @codelet graph function TimesTwo(inconst::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+    @codelet graph function TimesTwo(inconst::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
         outvec .= double.(inconst)
     end
 
-    @codelet graph function Sort(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+    @codelet graph function Sort(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
         outvec .= invec
         sort!(outvec)
     end
 
-    @codelet graph function Sin(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+    @codelet graph function Sin(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
         for idx in eachindex(outvec)
             @inbounds outvec[idx] = sin(invec[idx])
         end
     end
 
-    @codelet graph function Print(outvec::PoplarVec{Float32, Out})
+    @codelet graph function Print(outvec::PoplarVector{Float32, Out})
         @ipuprint "Hello, world!"
         @ipuprint "Titire tu" " patule" " recubans sub tegmine " "fagi"
         @ipuprint "The Answer to the Ultimate Question of Life, the Universe, and Everything is " 42
@@ -151,33 +151,33 @@ end
 function test_ipuprogram(device)
     N = 10
     input = randn(Float32, N)
-    outvec1 = PoplarVec{Float32, Out}(undef, N)
-    outvec2 = PoplarVec{Float32, Out}(undef, N)
-    outvec3 = PoplarVec{Float32, Out}(undef, N)
-    outvec4 = PoplarVec{Float32, Out}(undef, N)
-    outvec5 = PoplarVec{Float32, Out}(undef, N)
+    outvec1 = PoplarVector{Float32, Out}(undef, N)
+    outvec2 = PoplarVector{Float32, Out}(undef, N)
+    outvec3 = PoplarVector{Float32, Out}(undef, N)
+    outvec4 = PoplarVector{Float32, Out}(undef, N)
+    outvec5 = PoplarVector{Float32, Out}(undef, N)
     f(x) = cos(x)
     f′(x) = first(first(autodiff_deferred(Reverse, f, Active(x))))
     g(x) = tan(x)
     g′(x) = first(first(autodiff_deferred(Reverse, g, Active(x))))
 
     @ipuprogram device begin
-        function TimesTwo(inconst::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+        function TimesTwo(inconst::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
             outvec .= 2 .* inconst
         end
-        function Sort(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+        function Sort(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
             outvec .= invec
             sort!(outvec; rev=true)
         end
-        function Exp(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+        function Exp(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
             for idx in eachindex(outvec)
                 @inbounds outvec[idx] = exp(invec[idx])
             end
         end
-        function DiffCos(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+        function DiffCos(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
             outvec .= f′.(invec)
         end
-        function DiffTan(invec::PoplarVec{Float32, In}, outvec::PoplarVec{Float32, Out})
+        function DiffTan(invec::PoplarVector{Float32, In}, outvec::PoplarVector{Float32, Out})
             for idx in eachindex(outvec)
                 @inbounds outvec[idx] = g′(invec[idx])
             end
@@ -226,11 +226,11 @@ end
         end
     end
 
-    @testset "PoplarVec" begin
-        vec = PoplarVec{Float32, Out}(undef, 10)
+    @testset "PoplarVector" begin
+        vec = PoplarVector{Float32, Out}(undef, 10)
         @test vec.base == C_NULL
-        @test vec.size == 10
-        @test contains(repr(vec), r"PoplarVec{Float32,.*Out}")
+        @test vec.size == (10,)
+        @test contains(repr(vec), r"PoplarVector{Float32,.*Out}")
     end
 
     @testset "Printing to IPU" begin
