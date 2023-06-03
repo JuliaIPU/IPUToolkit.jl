2ebe5781bb392855b996f0eded6b2c96bb229a5d
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-06-04 00:53:35 +0200

    Move documentation to `Documenter.jl` and slim down `README.md` file

---
+++ docs/make.jl
@@ -0,0 +1,34 @@
+using Documenter, IPUToolkit.Poplar, IPUToolkit.IPUCompiler
+
+# Pretend to be GitHub Action to publish the documentation.  Note that in order to actually
+# publish you also need to have other secret environment variables , `GITHUB_ACTOR` and
+# `DOCUMENTER_KEY` set appropriately:
+#
+# * `GITHUB_TOKEN`: a GitHub Personal Access Token
+#   (https://github.com/settings/tokens?type=beta) with write access to the repo
+# * `GITHUB_ACTOR`: your GitHub username
+# * `DOCUMENTER_KEY`: generate with `DocumenterTools.genkeys`
+#   (https://documenter.juliadocs.org/stable/lib/public/#DocumenterTools.genkeys)
+#
+ENV["GITHUB_EVENT_NAME"] = "push"
+ENV["GITHUB_REPOSITORY"] = "github.com/giordano/julia-ipu.git"
+ENV["GITHUB_REF"] = "refs/heads/main"
+
+makedocs(
+    modules = [IPUToolkit],
+    sitename = "IPUToolkit",
+    pages    = [
+        "IPUToolkit" => "index.md",
+        "Poplar SDK" => "poplar.md",
+        "Writing codelets" => "compiler.md",
+    ],
+    strict = true,
+)
+
+deploydocs(
+    repo = "github.com/giordano/julia-ipu.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+    devbranch = "main",
+)
