664dd3bd86576e7cea2b9ef1b7aadc1219f39654
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-06-22 00:11:07 +0200

    Use different input parameters for each solver

--- examples/diffeq.jl
+++ examples/diffeq.jl
@@ -26,12 +26,19 @@ graph = Poplar.Graph(target)
 num_tiles = Int(Poplar.TargetGetNumTiles(target))
 tiles = 0:(num_tiles - 1)
 
-p = repeat([7f0, 28f0, 8f0 / 3f0], num_tiles)
-
+# Create a range of different input parameters
+p = let
+    σ = repeat(range(0f0; step=0.2f0, length=64); inner=23)
+    ρ = repeat(range(10f0; step=1f0, length=23); outer=64)
+    β = repeat([8f0 / 3f0], num_tiles)
+    zip(σ, ρ, β) |> Iterators.flatten |> collect
+end
+# Output arrays
 n = 10000
 u1 = Vector{Float32}(undef, n * num_tiles)
 u2 = Vector{Float32}(undef, n * num_tiles)
 
+# Define kernel
 @codelet graph function solve_lorenz(u1::VertexVector{Float32, Out},
                                      u2::VertexVector{Float32, Out},
                                      p::VertexVector{Float32, In})
@@ -46,20 +53,25 @@ u2 = Vector{Float32}(undef, n * num_tiles)
     return nothing
 end
 
+# Input and output tensors on the IPU
 p_ipu =  Poplar.GraphAddConstant(graph, p)
 u1_ipu = Poplar.GraphAddVariable(graph, Poplar.FLOAT(), UInt64[n * num_tiles], "u1")
 u2_ipu = Poplar.GraphAddVariable(graph, Poplar.FLOAT(), UInt64[n * num_tiles], "u2")
 
 prog = Poplar.ProgramSequence()
 
+# Run the codelet defined above on all tiles, with tensors evenly spread across all cores.
 add_vertex(graph, prog, tiles, solve_lorenz, u1_ipu, u2_ipu, p_ipu)
 
+# Prepare tensors read
 Poplar.GraphCreateHostRead(graph, "u1-read", u1_ipu)
 Poplar.GraphCreateHostRead(graph, "u2-read", u2_ipu)
 
+# Run the program
 engine = Poplar.Engine(graph, prog)
 Poplar.EngineLoadAndRun(engine, device)
 
+# Read the output tensors back to the CPU
 Poplar.EngineReadTensor(engine, "u1-read", u1)
 Poplar.EngineReadTensor(engine, "u2-read", u2)
 
