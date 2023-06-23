ef66d5d786df7d618dbd2b314a616207d6c78a64
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-06-23 18:06:04 +0200

    [docs] Remove hacks

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,4 @@
-# Activate docs environment, dev the package we want to build docs for, instantiate.
-using Pkg
-Pkg.activate(@__DIR__)
-Pkg.develop(PackageSpec(; path=dirname(@__DIR__)))
-Pkg.instantiate()
-Pkg.precompile()
-
-# Start building docs from here
 using Documenter, IPUToolkit, IPUToolkit.Poplar, IPUToolkit.IPUCompiler
-
-# Pretend to be GitHub Action to publish the documentation.  Note that in order to actually
-# publish you also need to have other secret environment variables , `GITHUB_ACTOR` and
-# `DOCUMENTER_KEY` set appropriately:
-#
-# * `GITHUB_TOKEN`: a GitHub Personal Access Token
-#   (https://github.com/settings/tokens?type=beta) with write access to the repo
-# * `GITHUB_ACTOR`: your GitHub username
-# * `DOCUMENTER_KEY`: generate with `DocumenterTools.genkeys`
-#   (https://documenter.juliadocs.org/stable/lib/public/#DocumenterTools.genkeys)
-#
-ENV["GITHUB_EVENT_NAME"] = "push"
-ENV["GITHUB_REPOSITORY"] = "github.com/giordano/IPUToolkit.jl.git"
-ENV["GITHUB_REF"] = "refs/heads/main"
 
 makedocs(
     modules = [Poplar, IPUCompiler],
