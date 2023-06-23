04241f2e1867d2f5bf41af958cee0e902247d530
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-06-23 13:45:08 +0200

    Replace `GraphAddVariable` with `similar` in DiffEq example

--- examples/diffeq.jl
+++ examples/diffeq.jl
@@ -3,8 +3,19 @@ using IPUToolkit.Poplar
 using DiffEqGPU
 using StaticArrays
 using OrdinaryDiffEq
-using Plots
 
+ENV["POPLAR_RUNTIME_OPTIONS"] = """{"target.hostSyncTimeout":"60"}"""
+IPUCompiler.PROGRESS_SPINNER[] = false
+
+device = Poplar.get_ipu_device()
+target = Poplar.DeviceGetTarget(device)
+graph = Poplar.Graph(target)
+prog = Poplar.ProgramSequence()
+
+num_tiles = Int(Poplar.TargetGetNumTiles(target))
+tiles = 0:(num_tiles - 1)
+
+# Define differential equation
 function lorenz(u, p, t)
     σ = p[1]
     ρ = p[2]
@@ -14,17 +25,6 @@ function lorenz(u, p, t)
     du3 = u[1] * u[2] - β * u[3]
     return SVector{3}(du1, du2, du3)
 end
-
-ENV["POPLAR_RUNTIME_OPTIONS"] = """{"target.hostSyncTimeout":"60"}"""
-
-IPUCompiler.PROGRESS_SPINNER[] = false
-
-device = Poplar.get_ipu_device()
-target = Poplar.DeviceGetTarget(device)
-graph = Poplar.Graph(target)
-
-num_tiles = Int(Poplar.TargetGetNumTiles(target))
-tiles = 0:(num_tiles - 1)
 
 # Create a range of different input parameters
 p = let
@@ -37,10 +37,12 @@ end
 n = 10000
 u1 = Vector{Float32}(undef, n * num_tiles)
 u2 = Vector{Float32}(undef, n * num_tiles)
+u3 = Vector{Float32}(undef, n * num_tiles)
 
 # Define kernel
 @codelet graph function solve_lorenz(u1::VertexVector{Float32, Out},
                                      u2::VertexVector{Float32, Out},
+                                     u3::VertexVector{Float32, Out},
                                      p::VertexVector{Float32, In})
     u0 = @SVector [1f0; 0f0; 0f0]
     svp = @inbounds SVector{3, Float32}(p)
@@ -49,23 +51,24 @@ u2 = Vector{Float32}(undef, n * num_tiles)
         DiffEqGPU.step!(integ, integ.t + integ.dt, integ.u)
         u1[idx] = integ.u[1]
         u2[idx] = integ.u[2]
+        u3[idx] = integ.u[3]
     end
     return nothing
 end
 
 # Input and output tensors on the IPU
 p_ipu =  Poplar.GraphAddConstant(graph, p)
-u1_ipu = Poplar.GraphAddVariable(graph, Poplar.FLOAT(), UInt64[n * num_tiles], "u1")
-u2_ipu = Poplar.GraphAddVariable(graph, Poplar.FLOAT(), UInt64[n * num_tiles], "u2")
-
-prog = Poplar.ProgramSequence()
+u1_ipu = similar(graph, u1, "u1")
+u2_ipu = similar(graph, u2, "u2")
+u3_ipu = similar(graph, u3, "u3")
 
 # Run the codelet defined above on all tiles, with tensors evenly spread across all cores.
-add_vertex(graph, prog, tiles, solve_lorenz, u1_ipu, u2_ipu, p_ipu)
+add_vertex(graph, prog, tiles, solve_lorenz, u1_ipu, u2_ipu, u3_ipu, p_ipu)
 
 # Prepare tensors read
 Poplar.GraphCreateHostRead(graph, "u1-read", u1_ipu)
 Poplar.GraphCreateHostRead(graph, "u2-read", u2_ipu)
+Poplar.GraphCreateHostRead(graph, "u3-read", u3_ipu)
 
 # Run the program
 engine = Poplar.Engine(graph, prog)
@@ -74,5 +77,6 @@ Poplar.EngineLoadAndRun(engine, device)
 # Read the output tensors back to the CPU
 Poplar.EngineReadTensor(engine, "u1-read", u1)
 Poplar.EngineReadTensor(engine, "u2-read", u2)
+Poplar.EngineReadTensor(engine, "u3-read", u3)
 
 Poplar.DeviceDetach(device)
