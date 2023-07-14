985da49941d572edca12a2785a9dd6c0d673e4db
Mosè Giordano <m.giordano@ucl.ac.uk>
2023-07-14 10:43:58 +0100

    Do manual loop unrolling in pi example
    [skip ci]

--- examples/pi.jl
+++ examples/pi.jl
@@ -1,6 +1,6 @@
 using IPUToolkit.IPUCompiler, IPUToolkit.Poplar
 
-ENV["POPLAR_RUNTIME_OPTIONS"] = """{"target.hostSyncTimeout":"60"}"""
+ENV["POPLAR_RUNTIME_OPTIONS"] = """{"target.hostSyncTimeout":"20"}"""
 
 IPUCompiler.KEEP_LLVM_FILES[] = true
 
@@ -8,8 +8,40 @@ device = Poplar.get_ipu_device()
 target = Poplar.DeviceGetTarget(device)
 graph = Poplar.Graph(target)
 
+function print_usage(unroll, n)
+    println(stderr, """
+                    Usage:
+                        julia $(@__FILE__) [loop unroll factor] [n]
+                    Default values are
+                        unroll factor: $(unroll)
+                        n: $(n)
+                    Examples:
+                        julia $(@__FILE__) 2
+                        julia $(@__FILE__) 4 729444
+                    """)
+end
+
 num_tiles = Int(Poplar.TargetGetNumTiles(target))
-n::UInt32 = typemax(UInt32) ÷ num_tiles
+n::UInt32, unroll::UInt32 = let
+    # Default values of n and the loop unrolling factor
+    n = typemax(UInt32) ÷ num_tiles
+    unroll = UInt32(1)
+    # Parse command line arguments, if any
+    if length(ARGS) ≤ 2
+        if length(ARGS) ≥ 1
+            unroll = parse(UInt32, ARGS[1])
+        end
+        if length(ARGS) == 2
+            n = parse(UInt32, ARGS[2])
+        end
+    else
+        print_usage(unroll, n)
+        exit(1)
+    end
+    # Make sure n is an integer multiple of the loop unrolling factor before returning
+    iszero(rem(n, unroll)) || error("n ($(n)) is not an integer multiple of unroll factor ($(unroll))")
+    n, unroll
+end
 num_steps::UInt32 = num_tiles * n
 slice::Float32 = 1 / num_steps
 
@@ -25,9 +57,13 @@ cycles = similar(ids)
 # user-friendly :)  And a top-level `@eval` is not _too_ bad.
 @eval function pi_kernel(i::T) where {T<:Integer}
     sum = 0f0
-    for j in (i * $(n)):((i + one(T)) * $(n) - one(T))
-        x = (j - 5f-1) * $(slice)
-        sum += 4 / (1 + x ^ 2)
+    for j in (i * $(n)):$(unroll):((i + one(T)) * $(n) - one(T))
+        # Do manual loop unrolling, sometimes this can be better than what the
+        # compiler can do.
+        Base.Cartesian.@nexprs $(Int64(unroll)) idx -> begin
+            x = (j + Float32(idx - 1) - 5f-1) * $(slice)
+            sum += 4f0 / (1f0 + x ^ 2)
+        end
     end
     return sum
 end
@@ -68,6 +104,7 @@ print("""
       Calculating PI using:
         $(num_steps) slices
         $(num_tiles) IPU tiles
+        loop unroll factor $(Int(unroll))
       Obtained value of PI: $(pi)
       Time taken: $(time) seconds ($(maximum(cycles)) cycles at $(round(tile_clock_frequency / 1e9; sigdigits=3)) GHz)
       """)
